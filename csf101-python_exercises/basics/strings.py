1607bb4d9a03c06cb05a42b077a8bbca8a3e6a35
pelde684 <02250360.cst@rub.edu.bt>
2025-08-06 19:50:18 +0530

    first commit

--- csf101-python_exercises/basics/strings.py
+++ csf101-python_exercises/basics/strings.py
@@ -6,3 +6,13 @@ greeting_f= f"hello, {name}!"
 print(greeting_f)
 name_length = len(name)
 print(name_length)
+age=19
+age_str= str(age)
+message="i am" + age_str + " years old."
+print(message)
+non_num_str = "Hello"
+try:
+    non_num_int = int(non_num_str)
+    print(non_num_int)
+except ValueError as e:
+    print(f"Error: {e}")
