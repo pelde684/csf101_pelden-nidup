71712ad3ba2850a03d164c772811c0d87731d47a
pelde684 <02250360.cst@rub.edu.bt>
2025-08-19 10:39:41 +0530

    first commit

--- pi.py
+++ pi.py
@@ -1,1 +1,18 @@
-print("kuzuzanpola")
+a, b=10, 5
+print(f"addition: {a + b}")
+
+a = 10
+b = 5
+print(f"positive:{a, b}")
+
+x = 5
+print(f"initial x:{x}")
+x +=5
+print(f"after x +=5:{x}")
+
+
+a, b= 10,5
+print(f"a==b:{a==b}")
+
+a, b=10,5
+print(f"a>b:{a>b}")
