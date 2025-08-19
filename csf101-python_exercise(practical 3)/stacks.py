7b527498720af6de5e89aa990d98bb0b53f9eade
pelde684 <02250360.cst@rub.edu.bt>
2025-08-19 15:04:37 +0530

    first commit

---
+++ csf101-python_exercise(practical 3)/stacks.py
@@ -0,0 +1,32 @@
+class stack:
+    def __init__(self):
+        self. items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+          raise IndexError("stack is empty")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("stack is empty")
+        
+    def size(self):
+        return len(self.items)
+    
+stack = stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.pop())
+print(stack.peek())
+print(stack.size())
