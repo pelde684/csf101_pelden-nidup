ab912af6d769d0c56364f123bb3418e2c92668ea
pelde684 <02250360.cst@rub.edu.bt>
2025-08-25 20:55:41 +0530

    first commit

---
+++ csf101-python_exercises(practical 4)/reverse linked list.py
@@ -0,0 +1,63 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverseList(head):
+    prev = None
+    current = head
+    while current is not None:
+        next_temp = current.next
+        current.next = prevprev = current
+        prev = currentcurrent = next_temp
+        return prev
+
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def mergeTwoLists(list1: ListNode, list2: ListNode) -> ListNode:
+    dummy = ListNode(0)
+    current = dummy
+    
+    while list1 and list2:
+        if list1.val <= list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
+    
+    if list1:
+        current.next = list1
+    if list2:
+        current.next = list2
+    
+    return dummy.next
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next  = next
+
+    def removeNthFromEnd(head: ListNode, n: int) ->ListNode:
+        dummy = ListNode(0)
+        dummy.next = head
+        fast = slow = dummy
+
+        for _ in range(n):
+            fast = fast.next
+
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+        return dummy.next
+        
+
